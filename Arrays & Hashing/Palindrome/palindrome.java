91f3d50199e240fcde543dfabfe2cca643b54589
tharun reddy <159301909+Tharun-0408@users.noreply.github.com>
2025-08-20 00:11:42 +0530

    Create palindrome.java

---
+++ Arrays & Hashing/Palindrome/palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+        int l = 0, r = n-1;
+        while(l<r){
+            while(l < r && !Character.isLetterOrDigit(arr[i])){
+                l++;
+            }
+            while(r > l && !Character.isLetterOrDigit(arr[j])){
+                r--;
+            }
+
+            if(Character.toLowerCase(arr[l]) != Character.toLowerCase(arr[r])){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    } 
+}
+
+// Time complexity = O(n)
+// Space complexity = O(1)
