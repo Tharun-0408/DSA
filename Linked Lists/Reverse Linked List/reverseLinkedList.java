3a1d89f9d3ed08b8b51e2437119ffdc3a2d585ab
tharun reddy <159301909+Tharun-0408@users.noreply.github.com>
2025-10-18 21:41:39 +0530

    Create reverseLinkedList.java

---
+++ Linked Lists/Reverse Linked List/reverseLinkedList.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+        
+    }
+}
+
+//Time Complexity = O(n)
+//Space Complexity = O(1)
